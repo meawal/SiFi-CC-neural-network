d6b7083a4418c3f67e082e72a48b11ab747b12b4
Awal Awal <awal.nova@gmail.com>
2021-08-10 15:31:34 +0200

    created the AI model with seperate network to each output

--- sificc_lib/__init__.py
+++ sificc_lib/__init__.py
@@ -6,5 +6,7 @@ from sificc_lib.Event import Event
 from sificc_lib.Simulation import Simulation
 from sificc_lib.DataModel import DataModel
 from sificc_lib.DataModelQlty import DataModelQlty
-from sificc_lib.AI import AI
+from sificc_lib.DataModelSep import DataModelSep
+from sificc_lib.AI import AI, MyCallback
 from sificc_lib.AIQlty import AIQlty
+from sificc_lib.AISep import AISep
